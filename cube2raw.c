ef9569afaefc5f571e5fe3e973dcb38943cdb62b
Thomas Haschka <haschka@gmail.com>
2022-10-30 23:51:16 +0100

    Update cube2raw.c
    quick fix with atan have to update description

--- cube2raw.c
+++ cube2raw.c
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
     for(index_y=0;index_y<sizey;index_y++) {
       for(index_z=0;index_z<sizez;index_z++) {
 	fscanf(cubefile,"%lf",&new);
-
+	new = atan(new);
 	bigtable[index_x+index_y*sizex+index_z*sizexBYsizey] = new;
 	if(new > max) {
 	  max = new;
